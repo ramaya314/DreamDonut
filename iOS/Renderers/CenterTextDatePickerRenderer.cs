6b4319e54271fdc129a4428325f8606a55a81dc4
Ricardo Amaya <ricdamayar@gmail.com>
2018-03-13 23:33:59 -0400

    formating

---
+++ iOS/Renderers/CenterTextDatePickerRenderer.cs
@@ -0,0 +1,24 @@
+﻿
+using UIKit;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+using DreamDonut.iOS.Renderers;
+
+[assembly: ExportRenderer(typeof(DatePicker), typeof(CenterTextDatePickerRenderer))]
+namespace DreamDonut.iOS.Renderers
+{
+    public class CenterTextDatePickerRenderer : DatePickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.TextAlignment = UITextAlignment.Center;
+            }
+        }
+    }
+}
